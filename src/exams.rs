9b5ae74879589eace4ed2b9ec2a8ca7b4c6606f6
Kilian Koeltzsch <me@kilian.io>
2017-01-14 00:38:25 +0100

    Add Exam type

--- src/exams.rs
+++ src/exams.rs
@@ -10,3 +10,16 @@ pub fn student_exams(year: u16, course: u16, degree: Degree) {
 pub fn prof_exams(prof: &str) {
     println!("Getting data for prof {}.", prof);
 }
+
+#[derive(Debug)]
+pub struct Exam {
+    pub title: String,
+    pub exam_type: String,
+    pub study_branch: String,
+    pub day: String,
+    pub start_time: String,
+    pub end_time: String,
+    pub examiner: String,
+    pub next_chance: String,
+    pub rooms: Vec<String>,
+}
