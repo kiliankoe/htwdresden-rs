192376c3aed3e967e961635663ea3ea3810a39b5
Kilian Koeltzsch <me@kilian.io>
2017-01-14 15:54:10 +0100

    move exams mod to subdir

--- src/exams/mod.rs
+++ src/exams/mod.rs
@@ -1,10 +1,24 @@
+use hyper::Client;
+use hyper::status::StatusCode;
+
 use Degree;
 
 pub fn student_exams(year: u16, course: u16, degree: Degree) {
-    println!("Getting data for course {} in {} with degree {}.",
+    println!("Getting exams for course {} in {} with degree {}.",
              course,
              year,
              degree.short());
+
+    let client = Client::new();
+    let url = format!("http://www2.htw-dresden.de/~app/API/GetExams.\
+                       php?StgJhr={year}&Stg={course}&AbSc={degree}",
+                      year = year,
+                      course = course,
+                      degree = degree.short());
+    println!("{}", url);
+
+    let res = client.get(&url).send();
+    println!("{:?}", res);
 }
 
 pub fn prof_exams(prof: &str) {
