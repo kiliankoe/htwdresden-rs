5a387eb897d5034bcc27fe7b58b805295581ceaf
Kilian Koeltzsch <me@kilian.io>
2017-01-16 21:49:06 +0100

    trim whitespace from secret includes

--- examples/grades.rs
+++ examples/grades.rs
@@ -3,7 +3,7 @@ extern crate htwdresden;
 use htwdresden::{Login, grades};
 
 fn main() {
-    let login = Login::new(include_str!("secret_login"),
-                           include_str!("secret_password"));
+    let login = Login::new(include_str!("secret_login").trim(),
+                           include_str!("secret_password").trim());
     grades::get_courses(&login);
 }
