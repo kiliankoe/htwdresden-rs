a572360326fda98223b5494c16c923c4ca99688a
Kilian Koeltzsch <me@kilian.io>
2017-01-20 14:08:11 +0100

    read example grades from env

--- examples/grades.rs
+++ examples/grades.rs
@@ -3,12 +3,16 @@ extern crate htwdresden;
 use htwdresden::{Login, Course, Grade};
 
 fn main() {
-    let login = Login::new(include_str!("secret_login").trim(),
-                           include_str!("secret_password").trim());
+    let login = option_env!("LOGIN").unwrap_or("");
+    let password = option_env!("PASSWORD").unwrap_or("");
+    let login = Login::new(login, password);
+
     if let Ok(courses) = Course::get(&login) {
         println!("{:?}", courses);
 
         let grades = Grade::get(&login, &courses[0]).unwrap();
         println!("{:?}", grades);
+    } else {
+        println!("Couldn't find courses. Wrong login perhaps?");
     }
 }
