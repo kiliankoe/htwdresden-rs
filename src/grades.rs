5710d750904a6b4132881575826c4d7babd3a273
Kilian Koeltzsch <me@kilian.io>
2017-05-05 17:01:57 +0200

    base for richer grade type

--- src/grades.rs
+++ src/grades.rs
@@ -1,8 +1,11 @@
 use Error;
 use Login;
+use Year;
 
+use std::fmt;
 use std::collections::HashMap;
 use reqwest;
+use chrono;
 
 /// A course of studies.
 #[derive(Deserialize, Debug)]
@@ -51,9 +54,8 @@ impl Course {
     }
 }
 
-/// A grade, hopefully a good one.
 #[derive(Deserialize, Debug)]
-pub struct Grade {
+struct RawGrade {
     #[serde(rename = "PrNr")]
     pub exam_nr: String,
     #[serde(rename = "Status")]
@@ -75,9 +77,141 @@ pub struct Grade {
     #[serde(rename = "PrForm")]
     pub exam_form: String,
     #[serde(rename = "Vermerk")]
-    pub comment: String,
+    pub annotation: String,
     #[serde(rename = "EctsGrade")]
     pub ects_grade: String,
+}
+
+pub enum Status {
+    /// AN = Angemeldet
+    SignedUp,
+    /// BE = bestanden
+    Passed,
+    /// EN = endgültig nicht bestanden
+    Failed,
+    /// NB = nicht bestanden
+    UltimatelyFailed,
+}
+
+impl Status {
+    fn via(string: &str) -> Option<Self> {
+        match string {
+            "AN" => Some(Status::SignedUp),
+            "BE" => Some(Status::Passed),
+            "NB" => Some(Status::Failed),
+            "EN" => Some(Status::UltimatelyFailed),
+            _    => None,
+        }
+    }
+}
+
+impl fmt::Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let val = match self {
+            SignedUp         => "Angemeldet",
+            Passed           => "Bestanden",
+            Failed           => "Nicht bestanden",
+            UltimatelyFailed => "Endgültig nicht bestanden",
+        };
+        write!(f, "{}", val)
+    }
+}
+
+pub enum Annotation {
+    /// a = anerkannt
+    Accredited,
+    /// k = krank
+    Sick,
+    /// e = abgemeldet
+    SignedOut,
+    /// g = gesperrt
+    Blocked,
+    /// nz = nicht zugelassen
+    NotPermitted,
+    /// 5ue = unentschuldigt gefehlt
+    UnexcusedAbsence,
+    /// 5na = nicht angetreten - Fristüberschreitung
+    NotReported,
+    /// kA = kein Antrag 2. Wiederholungsprüfung gestellt
+    NoSecondAttemptApplication,
+    /// PFV = Freiversuch
+    FreeAttempt,
+    /// mE = mit Erfolg
+    WithSuccess,
+    /// N = nicht bestanden (unbenotete Leistung)
+    Failed,
+    /// VPo = Vorprakt. offen
+    OpenPracticalCourse,
+    /// f = freiwilliger Termin nicht wahrgenommen
+    NoShowOptionalAppointment,
+    /// uV = unter Vorbehalt
+    WithReservation,
+}
+
+impl Annotation {
+    fn via(string: &str) -> Option<Self> {
+        match string {
+            "a"   => Some(Annotation::Accredited),
+            "k"   => Some(Annotation::Sick),
+            "e"   => Some(Annotation::SignedOut),
+            "g"   => Some(Annotation::Blocked),
+            "nz"  => Some(Annotation::NotPermitted),
+            "5ue" => Some(Annotation::UnexcusedAbsence),
+            "5na" => Some(Annotation::NotReported),
+            "kA"  => Some(Annotation::NoSecondAttemptApplication),
+            "PFV" => Some(Annotation::FreeAttempt),
+            "mE"  => Some(Annotation::WithSuccess),
+            "N"   => Some(Annotation::Failed),
+            "VPo" => Some(Annotation::OpenPracticalCourse),
+            "f"   => Some(Annotation::NoShowOptionalAppointment),
+            "uV"  => Some(Annotation::WithReservation),
+            _     => None,
+        }
+    }
+}
+
+pub enum Semester {
+    Winter(Year),
+    Summer(Year),
+}
+
+impl Semester {
+    fn via(string: &str) -> Option<Self> {
+        let year_str = &string[0..3];
+        let year: Year = year_str.to_owned().parse::<u16>().unwrap_or_default();
+        let id = string.chars().nth(4).unwrap_or('0');
+
+        match id {
+            '1' => Some(Semester::Winter(year)),
+            '2' => Some(Semester::Summer(year)),
+            _   => None,
+        }
+    }
+}
+
+impl fmt::Display for Semester {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &Semester::Winter(year) => write!(f, "WS {}", year),
+            &Semester::Summer(year) => write!(f, "SS {}", year),
+        }
+    }
+}
+
+/// A grade, hopefully a good one.
+pub struct Grade {
+    pub exam_nr: String,
+    pub status: Option<Status>,
+    pub ects_credits: f32,
+    pub exam_txt: String,
+    pub semester: Option<Semester>,
+    pub try_count: i8,
+    pub exam_date: Option<chrono::DateTime<chrono::UTC>>,
+    pub grade: Option<f32>,
+    pub publish_date: Option<chrono::DateTime<chrono::UTC>>,
+    pub exam_form: String, // should be it's own enum type, not sure about possible values though
+    pub annotation: Option<Annotation>,
+    pub ects_grade: Option<String>, // not sure what this should be, probably f32
 }
 
 impl Grade {
@@ -112,8 +246,34 @@ impl Grade {
             .form(&map)
             .send()?
             .json()
-            .map(|response: Vec<Grade>| response)?;
+            .map(|response: Vec<RawGrade>| response)?
+            .into();
 
         Ok(grades)
+    }
+}
+
+impl From<RawGrade> for Grade {
+    fn from(raw: RawGrade) -> Self {
+        Grade {
+            exam_nr:      raw.exam_nr,
+            status:       Status::via(&raw.status),
+            ects_credits: raw.ects_credits.parse::<f32>().unwrap_or(0.0),
+            exam_txt:     raw.exam_txt,
+            semester:     Semester::via(&raw.semester),
+            try_count:    raw.try_count.parse::<i8>().unwrap_or(0),
+            exam_date:    chrono::DateTime::parse_from_str(&raw.exam_date, "%d.%m.%Y").ok(),
+            grade:        Some(1.0), // TODO
+            publish_date: chrono::DateTime::parse_from_str(&raw.publish_date, "%d.%m.%Y").ok(),
+            exam_form:    raw.exam_form,
+            annotation:   Annotation::via(&raw.annotation),
+            ects_grade:   Some(raw.ects_grade),
+        }
+    }
+}
+
+impl fmt::Display for Grade {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {} {}", self.exam_txt, self.grade.unwrap_or(0.0), self.status.unwrap_or(Status::SignedUp)) // FIXME
     }
 }
